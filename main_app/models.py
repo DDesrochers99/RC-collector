914b1abf424abbdb241049ef803c58f7e36baf42
ddesrochers99 <desrochersdale@gmail.com>
2023-08-01 15:21:05 -0700

    Step 1

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class RemoteControlVehicle(models.Model):
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.id} - {self.model}"
--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 
-remote_control_vehicles = [
-    {'brand': 'Traxxas', 'model': 'Slash 4x4', 'description': 'High-performance off-road truck', 'price': 399.99},
-    {'brand': 'Redcat Racing', 'model': 'Everest 10', 'description': 'Rock crawler with excellent traction', 'price': 299.99},
-    {'brand': 'HPI Racing', 'model': 'Savage XS Flux', 'description': 'Compact and powerful monster truck', 'price': 349.99},
-]
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
 
-def index(request):
-    return render(request, "index.html")
+def about(request):
+    return render(request, "about.html")
+
+
+def rc_index(request):
+    return render(request, "rc/index.html")
+
+def index_view(request):
+    vehicles = RemoteControlVehicle.objects.all()
+    return render(request, 'rc/index.html', {'remote_control_vehicles': vehicles})
