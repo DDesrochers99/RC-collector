ba061036606fe2076c816796921327266ff50950
ddesrochers99 <desrochersdale@gmail.com>
2023-08-03 12:11:26 -0700

    Part 3

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('about/', views.about, name='about'),
-    path('rc/', views.rc_index, name='index'),
+    # path('rc/', views.rc_index, name='index'),
     path('rc/<int:rc_id>/', views.rc_detail, name='detail'),
+    path('rc/', views.RemoteControlVehicleList.as_view(), name='rc_index'),
+    path('rc/<int:pk>/update/', views.RemoteControlVehicleUpdate.as_view(), name='rc_update'),
+    path('rc/<int:pk>/delete/', views.RemoteControlVehicleDelete.as_view(), name='rc_delete'),
+    path('rc/create/', views.RemoteControlVehicleCreate.as_view(), name='rc_create'),
+    
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import RemoteControlVehicle
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
 # Create your views here.
 def home(request):
@@ -9,11 +10,29 @@ def about(request):
     return render(request, "about.html")
 
 
-def rc_index(request):
-    rc = RemoteControlVehicle.objects.all()
-    print(rc)
-    return render(request, "rc/index.html", {"rc": rc})
+# def rc_index(request):
+#     rc = RemoteControlVehicle.objects.all()
+#     return render(request, "rc/index.html", {"rc": rc})
 
 def rc_detail(request, rc_id):
     rc_instance = RemoteControlVehicle.objects.get(id=rc_id)
     return render(request, 'rc/details.html', {'rc': rc_instance})
+
+
+class RemoteControlVehicleList(ListView):
+    model = RemoteControlVehicle
+
+
+class RemoteControlVehicleCreate(CreateView):
+    model = RemoteControlVehicle
+    fields = "__all__"
+
+
+class RemoteControlVehicleUpdate(UpdateView):
+    model = RemoteControlVehicle
+    fields = "__all__"
+
+
+class RemoteControlVehicleDelete(DeleteView):
+    model = RemoteControlVehicle
+    success_url = "/rc"
